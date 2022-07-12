3cba9659b42609ba89fb15bd0126e675d0d7ff20
s1290216 <s1290216@u-aizu.ac.jp>
2022-07-12 21:00:03 +0900

    HoT: initial revision

---
+++ Ex04/Ex04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tossing a coin...");
+            Random r = new System.Random();
+            int head = 0;
+            int tail = 0;
+            for(int i = 0; i < 3; i++)
+            {
+                if(r.Next() % 2 == 0)
+                {
+                    Console.WriteLine("Round {0}: Heads", i + 1);
+                    head++;
+                }
+                else
+                {
+                    Console.WriteLine("Round {0}: Tails", i + 1);
+                    tail++;
+                }
+            }
+            Console.WriteLine("Heads: {0}, Tails: {1}", head, tail);
+        }
+    }
+}
